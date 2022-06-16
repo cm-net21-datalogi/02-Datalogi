01f6e4c30cd75afcac83053844880bf241e3e670
David Andersson <lejonmanen@gmail.com>
2022-06-16 13:46:23 +0200

    Add project files.

---
+++ Datalogi-2/Program.cs
@@ -0,0 +1,66 @@
+﻿
+//int f4 = Factorial(4);
+//Console.WriteLine("4! == " + f4);
+// Facit: 4 * 3 * 2 * 1 == 4 * 6 == 24
+
+static int Factorial(int n)
+{
+	if (n <= 1)
+		return 1;
+	return n * Factorial(n - 1);
+}
+
+static void PrintUpTo(int max)
+{
+	/*if(max < 1)
+	{
+		return;
+	}
+	else*/
+	if(max > 0)
+	{
+		PrintUpTo(max - 1);
+		Console.WriteLine(max);
+	}
+}
+
+//PrintUpTo(7);
+
+
+//double result = CalculateInterest(100, 50);
+//Console.WriteLine("Balance after 50 year is " + result);
+
+
+static double CalculateInterest(double balance, int years)
+{
+	const double interestRate = 0.05;
+	double interestThisYear = balance * interestRate;
+
+	if(years == 1)
+	{
+		return balance + interestThisYear;
+	}
+	else
+	{
+		double newBalance = balance + interestThisYear;
+		return CalculateInterest(newBalance, years - 1);
+	}
+}
+
+
+
+static int ArraySum(int[] array, int startIndex = 0)
+{
+	if (startIndex == array.Length - 1)
+	{
+		return array[startIndex];
+	}
+	else
+	{
+		return array[startIndex] + ArraySum(array, startIndex + 1);
+	}
+}
+
+int[] testArray = new int[] { 2, 3, 5 };
+int sum = ArraySum(testArray);
+Console.WriteLine("Sum of test array is: " + sum);
